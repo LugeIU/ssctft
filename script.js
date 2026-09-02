cf04ac5802adac8aa06587c5f70f4c81a6812049
LugeIU <45872004+LugeIU@users.noreply.github.com>
2026-09-02 22:17:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// 1. QUAN TRỌNG: Hãy xóa link mẫu dưới đây và dán link Worker của bạn vào
-// Ví dụ: "https://tft-proxy.ten-cua-ban.workers.dev"
 const CLOUDFLARE_WORKER_URL = "https://tft-proxy.nghiepdt2911.workers.dev"; 
-
 
 async function loadRiotData() {
     const tbody = document.getElementById('leaderboard-body');
@@ -13,10 +10,9 @@ async function loadRiotData() {
             throw new Error(`Mã lỗi: ${response.status}. Hãy kiểm tra lại API Key.`);
         }
 
-        // Dữ liệu nhận được giờ là một mảng 15 người đã có sẵn Tên
         const players = await response.json();
         
-        tbody.innerHTML = ''; // Xóa chữ "Đang tải..."
+        tbody.innerHTML = ''; 
 
         players.forEach((player, index) => {
             const totalGames = player.wins + player.losses;
@@ -25,10 +21,7 @@ async function loadRiotData() {
             const tr = document.createElement('tr');
             tr.innerHTML = `
                 <td class="rank">#${index + 1}</td>
-                
-                <!-- Hiển thị player.name thay vì player.summonerName -->
                 <td><strong>${player.name}</strong></td>
-                
                 <td class="tier">Thách Đấu</td>
                 <td>${player.leaguePoints} LP</td>
                 <td>${winRate} (${player.wins}W - ${player.losses}L)</td>
